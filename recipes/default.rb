e507a413ad1a4168f4d38429667973f659601b08
alexai <yai@dfengg.com>
2014-01-17 10:21:19 +0800

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,3 @@
-#
 # Cookbook Name:: mercurial
 # Recipe:: default
 #
@@ -6,6 +5,27 @@
 #
 # Mailto: yai@dfengg.com
 #
+#Initilizate database fisheye
+cookbook_file "/root/init.sql" do
+   source "init.sql"
+   owner  "root"
+   mode   "0755"
+   action :create
+end
+
+#Download python hglibs.
+remote_file "/root/#{node.ag.hg.pylib}" do
+        source "http://mercurial.selenic.com/release/python-hglib/python-hglib-0.1.tar.gz"
+        action :create_if_missing
+        notifies :run, "script[install_hglib]", :immediately
+end 
+
+#Download Mercurial-2.2.3 source ball.
+remote_file "/root/#{node.ag.hg.src_pkg}" do
+        source "http://mercurial.selenic.com/release/mercurial-2.2.3.tar.gz"
+        action :create_if_missing
+        notifies :run, "script[install_hg223]", :immediately
+end
 
 #Install  python-hglibs from source.
 script "install_hglib" do
@@ -17,21 +37,6 @@ script "install_hglib" do
                 tar zxvf #{node.ag.hg.pylib}
                 cd python-hglib-0.1
                 python setup.py install        
-EOH
-end
-
-#Install Mysql-server-5.5.20
-script "install_mysql" do
-        interpreter "bash"
-        user "root"
-        cwd "/root"
-        action :nothing
-        code <<-EOH
-                rpm -qa |grep MySQL-server
-                [ "$?" -ne "0" ] && yum install -y /root/#{node.ag.mysql.rpm} || echo "Mysql-server installed"
-                /etc/init.d/mysql start
-                chkconfig mysql on
-		mysql -uroot < /root/init.sql
 EOH
 end
 
@@ -53,31 +58,16 @@ script "install_hg223" do
 EOH
 end
 
-#Download mysql-server
-remote_file "/root/#{node.ag.mysql.rpm}" do
-        source "http://downloads.mysql.com/archives/mysql-5.5/MySQL-server-5.5.20-1.rhel4.x86_64.rpm"
-        action :create_if_missing
-        notifies :run, "script[install_mysql]", :immediately
-end
-
-#Initilizate database fisheye
-cookbook_file "/root/init.sql" do
-   source "init.sql"
-   owner  "root"
-   mode   "0755"
-   action :create
-end
-
-#Download python hglibs.
-remote_file "/root/#{node.ag.hg.pylib}" do
-        source "http://mercurial.selenic.com/release/python-hglib/python-hglib-0.1.tar.gz"
-        action :create_if_missing
-        notifies :run, "script[install_hglib]", :immediately
-end 
-
-#Download Mercurial-2.2.3 source ball.
-remote_file "/root/#{node.ag.hg.src_pkg}" do
-        source "http://mercurial.selenic.com/release/mercurial-2.2.3.tar.gz"
-        action :create_if_missing
-        notifies :run, "script[install_hg223]", :immediately
+#Install Mysql-server-5.5.20
+script "install_mysql" do
+        interpreter "bash"
+        user "root"
+        cwd "/root"
+        action :nothing
+        code <<-EOH
+                yum install -y mysql mysql-server
+                /etc/init.d/mysqld start
+                chkconfig mysqld on
+		mysql -uroot < /root/init.sql
+EOH
 end
